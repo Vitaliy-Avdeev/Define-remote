020cc10c42dc36884e4cb9471410d56207a963ee
Vitaliy-Avdeev <avd.vit78@gmail.com>
2024-10-07 20:36:11 +0300

    The code is initialized

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,102 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        var taskOne = ("Задача №1");
+        System.out.println(taskOne);
+
  var dog = 8.0;
- System.out.println(dog + " Собака");
+ System.out.println(dog);
 
  var cat = 3.6;
- System.out.println(cat + " Кот");
+ System.out.println(cat);
 
  var paper = 763789;
- System.out.println(paper + " Бумага");
+ System.out.println(paper);
+
+ var taskTwo = ("Задача №2");
+ System.out.println(taskTwo);
+
+ dog = dog + 4;
+ System.out.println(dog);
+
+ cat = cat + 4;
+ System.out.println(cat);
+
+ paper = paper + 4;
+ System.out.println(paper);
+
+ var taskThree = ("Задача №3");
+ System.out.println(taskThree);
+
+ var dogAnimal = dog - 3.5;
+ System.out.println(dogAnimal);
+
+ var catAnimal = cat - 1.6;
+ System.out.println(catAnimal);
+
+ var paperMaterial = paper - 7639;
+ System.out.println(paperMaterial);
+
+ var taskFour = ("Задача №4");
+ System.out.println(taskFour);
+
+ var friend = 19;
+ System.out.println(friend);
+
+ friend = friend +2;
+ System.out.println(friend);
+
+ friend = friend / 7;
+ System.out.println(friend);
+
+ var taskFive = ("Задача №5");
+ System.out.println(taskFive);
+
+ var frog = 3.5;
+ System.out.println(frog);
+
+ frog = frog * 10;
+ System.out.println(frog);
+
+ frog = frog / 3.5;
+ System.out.println(frog);
+
+ frog = frog + 4;
+ System.out.println(frog);
+
+ var taskSix = ("Задача №6");
+ System.out.println(taskSix);
+
+ var boxerOne = 78.2;
+         var boxerTwo = 82.7;
+
+         var lotBoxer = boxerOne + boxerTwo;
+         System.out.println(lotBoxer + " кг." + " - Общая масса двух бойцов.");
+
+         var differenceBoxer = boxerOne - boxerTwo;
+         System.out.println(differenceBoxer + " кг.");
+
+         var taskSeven = ("Задача №7");
+         System.out.println(taskSeven );
+
+         var overload = (boxerTwo % boxerOne);
+         System.out.println("Разница в весе между двумя бойцами " + overload + " кг.");
+
+         var taskEight = ("Задача №8");
+         System.out.println(taskEight);
+
+         var timeTotal = 640;
+         var timeOperator = 8;
+         var numberOperators = timeTotal / timeOperator;
+         System.out.println("Всего работников в компании - " + numberOperators + " человек.");
+
+         var numberOperatorsNew = numberOperators + 94;
+         var timeOperatorNew = timeTotal / numberOperatorsNew;
+         System.out.println("Если в компании работает " + numberOperatorsNew + " человек, то всего " + timeOperatorNew + " часа работы может быть поделено между сотрудниками.");
+
+
+
+
+
     }
 }
